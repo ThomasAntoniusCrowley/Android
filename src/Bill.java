129fefe8ffceb6766e60fc23193973f0aab1fbcf
Jordan Day <jordanday_@hotmail.com>
2016-02-22 23:28:10 +0000

    Completed Bill class with comments

--- src/Bill.java
+++ src/Bill.java
@@ -1,16 +1,22 @@
-
+/**
+ * Created by jordan.
+ *
+ * Class to represent a bill for a meal.
+ */
 public class Bill {
 
 	private Item[] bill;
 	private double finalPrice;
 	
 	//private Item currentItem;
-		
+
+	//Creates a bill object, which is a list of billSize Item objects
 	public Bill(int billSize) {
 		bill = new Item[billSize];
 		//addVAT();
 	}
 
+	//Adds a new Item object at the next free element of the bill, if possible
 	public void addItem(Item item) {
 		boolean nextFree = false;
 		int currentElement = 0;
@@ -31,7 +37,8 @@ public class Bill {
 			System.out.println("Error: " + e);
 		}
 	}
-	
+
+	//Adds VAT to get a final price (to the penny)
 	public void addVAT() {
 		double itemTotal = 0;
 		for (Item item : bill) {
@@ -39,13 +46,13 @@ public class Bill {
 				itemTotal += item.getThisPrice();
 			}
 		}
-		System.out.println(itemTotal);
 		finalPrice = (itemTotal * 1.2);
 		finalPrice = Math.round(finalPrice * 100.0) / 100.0;
-		System.out.println(finalPrice);
+
 		
 	}
 
+	//Removes an item based on the input number, reallocates affected Items
 	public void removeItem(int itemNo) {
 		try {
 			for (int i = itemNo - 1; i < bill.length -1; ++i) {
@@ -55,10 +62,13 @@ public class Bill {
 		}
 		
 		catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Error: could not remove item. Please restart bill entries.");
+			System.out.println("Error: could not remove item. " +
+					"Please restart bill entries.");
 		}
 	}
-	
+
+	//Returns a string of each item in the bill, formatted
+	//appropriately and the total price with VAT
 	@Override public String toString() {
 		String billString = "";
 		for (Item thisItem : bill) {
@@ -72,25 +82,17 @@ public class Bill {
 				billString += "\n";
 			}
 		}
-		return billString.trim();
+
+		billString += "\n";
+		billString += "Total including VAT: £";
+		billString += finalPrice;
+
+		return billString;
 	}
 	
 	static public void main(String[] args) {
-		Bill thisBill = new Bill(3);
-		//System.out.println(thisBill.toString());
-		
-		Item item1 = new Item("chicken", "Food", "Meat", 12.23);
-		Item item2 = new Item("tuna", "Food", "Fish", 12.24);
-		Item item3 = new Item("rioja", "Drink", "Wine", 12.25);
-		
-		thisBill.addItem(item1);	
-		System.out.println(thisBill.toString());
-		
-		thisBill.addItem(item2);
-		thisBill.addItem(item3);	
-		System.out.println(thisBill.toString());
-		
-		thisBill.addVAT();
+		//Don't forget to call addVAT(); on bill once finished creating it
+
 	}
 }
 
